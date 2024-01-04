793fc2b79a87d2577909922d9eee3ef74f9d4161
pavel <pavel@gmail.com>
2024-01-04 11:16:15 +0300

    1

--- base64_coding.py
+++ base64_coding.py
@@ -5,3 +5,9 @@ import base64
 def encode_file(file):
   file_content = file.read()
   return base64.b64encode(file_content)
+
+if __name__ == "__main__":
+    
+    filename = '/home/pavel/Pictures/space_001.jpg'
+    file = open(filename, 'r', encoding='utf8')
+    print(encode_file(file))
